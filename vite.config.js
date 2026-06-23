008f8ef990445b904bedf5e1ba89e4afce438b8b
Sakhawat Ali <msakhawatali131@gmail.com>
2026-06-23 11:13:41 +0500

    Fix Vite deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/react-refresh'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio_with_react.js/',
   plugins: [
     react(),
     tailwindcss(),
