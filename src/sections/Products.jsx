bad2a711ede9efdb808ffeb902ef0781e951a2d6
Sakhawat Ali <msakhawatali131@gmail.com>
2026-07-20 12:24:25 +0500

    projects section contiune

--- src/sections/Products.jsx
+++ src/sections/Products.jsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import { useEffect, useRef, useState, useMemo } from "react"
 
-const IntroAnimation = () => {
-  return (
-    <div className='w-full h-screen'>
-      Products
-    </div>
+import img1 from "../assets/img1.JPG"
+import img2 from "../assets/img2.JPG"
+import img3 from "../assets/img3.JPG"
+import photo1 from "../assets/photo1.JPG"
+import photo2 from "../assets/photo2.PNG"
+import photo3 from "../assets/photo3.png"
+import { motion, AnimatePresence, useMotionValueEvent, useScroll } from "framer-motion"
+import { h3 } from "framer-motion/client"
+
+
+const useIsMobile = (query = "(max-width : 639px)") => {
+  const [isMobile , setIsMobile] = useState(
+    typeof window !== "undefined" && window.matchMedia(query).matches
   )
+  useEffect(() => {
+    if(typeof window === "undefined") return;
+    const mql = window.matchMedia(query);
+    const handler = (e) => setIsMobile(e.matches);
+
+    mql.addEventListener("change", handler);
+    setIsMobile(mql.matches);
+    return () => mql.removeEventListener("change" , handler)
+  }, [query])
+  return isMobile;
 }
 
-export default IntroAnimation
+
+
+export default function Projects(){
+  const isMobile = useIsMobile();
+  const sceneRef = useRef(null);
+
+  const projects = useMemo(
+    () => [
+    {
+    title: "nk studio",
+    link: "https://www.nk.studio/",
+    bgColor: "#0d4d3d",
+    image: isMobile ? photo1 : img1, // use mobile or desktop image
+    },
+    {
+    title: "Gamily",
+    link: "https://gamilyapp.com/",
+    bgColor: "#3884d3",
+    image: isMobile ? photo2 : img2,
+    },
+    {
+    title: "Hungry Tiger",
+    link: "https://www.eathungrytiger.com/",
+    bgColor: "#dc9317",
+    image: isMobile ? photo3 : img3,
+
+        },
+      ],
+      [isMobile] // re-run only when `isMobile` changes
+    );
+
+    const {scrollYProgress} = useScroll({
+      target:sceneRef
+      offset : ["start start" , "end end"]
+    })
+    const thresholds = projects.map((_,i) => (i+1)/projects.length)
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useMotionValueEvent(scrollYProgress, "change" , (Y) => {
+      const idx = thresholds.findIndex((t) => v <= t);
+      setActiveIndex(idx === -1 ? thresholds.length -1 : idx)
+    });
+
+  const activeProject = projects[activeIndex];
+  return (
+    <section id="projects" 
+    ref = {sceneRef}
+    className="relative text-white"
+    style={{
+      height : `${100*projects.length}vh`,
+      backgroundColor : activeProject.bgColor,
+      transition : "background-color 40ms ease"
+    }}
+    >
+      <div className="stick top-0 h-screen flex flex-col items-center justify-center">
+        <h2 className={`text-3xl font-semibold z-10 text-center ${
+          isMobile ?  "mt-4" : "mt-8"
+        }`}>
+          My Work
+        </h2>
+        <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-500 ${
+          activeIndex === idx ? "opacity-100 z-20" : "opacity-0 z-0 sm:z-10"
+        }`}
+        
+        style={{width : "85%", maxWidth: "1200px"}}
+        >
+          <AnimatePresence mode="wait">
+            {activeIndex == idx && (
+              <motion.h3 key={project.title}
+              initial = {{opacity : 0 , y: -30}}
+              animate = {{opacity : 1, y:0}}
+              exit = {{opacity : 0, y:30}}
+              transition={{duration:0.5, ease : "easeOut"}}
+              className={`block text-center text-[clamp(2rem, 6vw, 5rem)] text-white/95 sm:absolute sm:-top-20 sm:left-[35%] lg:left-[-5%] sm:mb-0
+                italic font-semibold ${
+                  isMobile ? "-mt-24" : ""
+                }
+                `}
+              >
+                {project.title}
+
+              </motion.h3>
+            )}
+
+          </AnimatePresence>
+
+        </div>
+      </div>
+
+    </section>
+  )
+}
