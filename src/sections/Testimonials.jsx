4088343dcc0d8032e43a7601a8b38f4a9b3de4f2
Sakhawat Ali <msakhawatali131@gmail.com>
2026-09-19 15:12:21 +0500

    Fix image import case for Linux build

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -2,10 +2,10 @@ import { motion } from "framer-motion"
 
 
 import { ImOffice } from "react-icons/im"
-import m1 from "../assets/m1.png"
-import m2 from "../assets/m2.png"
-import w1 from "../assets/w1.png"
-import w2 from "../assets/w2.png"
+import m1 from "../assets/m1.PNG";
+import m2 from "../assets/m2.PNG";
+import w1 from "../assets/w1.PNG";
+import w2 from "../assets/w2.PNG";
 
 const testimonials = [
   {
